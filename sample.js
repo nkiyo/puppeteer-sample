45279726f80e6510c4df8790ce164ea2af941513
nkiyo <hoge@hoge.com>
2020-01-29 11:03:48 +0900

    Add input using xpath

--- sample.js
+++ sample.js
@@ -4,6 +4,13 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://google.com');
+
+  // xpath で選択した部品に文字入力
+  const searchInputs = await page.$x('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input');
+  await searchInputs[0].type('1234');
+
+  // TODO xpath で選択した部品をクリック
+
   await page.screenshot({path: 'example.png'});
   await page.pdf({path: 'example.pdf', format: 'A4'});
 
