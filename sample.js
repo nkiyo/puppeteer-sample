d1b32862e30014b50d798ff2d5a68f4be1060cdc
nkiyo <hoge@hoge.com>
2020-01-29 11:09:07 +0900

    up

--- sample.js
+++ sample.js
@@ -4,12 +4,18 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://google.com');
+  // TODO ネット繋がってない場合は検出したい
 
+  // 検索窓にキーワードを入力する
   // xpath で選択した部品に文字入力
   const searchInputs = await page.$x('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input');
   await searchInputs[0].type('1234');
 
-  // TODO xpath で選択した部品をクリック
+  // 検索ボタンを押下する
+  // xpath で選択した部品をクリック
+  const searchButtons = await page.$x('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]');
+  await searchButtons[0].click();
+  await page.waitForNavigation();
 
   await page.screenshot({path: 'example.png'});
   await page.pdf({path: 'example.pdf', format: 'A4'});
